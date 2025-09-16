1aa381479a0b6d47537d2cef0940335cfb847ad3
pedroalf06 <38568867+pedroalf06@users.noreply.github.com>
2025-09-15 22:31:38 -0500

    ArrayList Infinito
    En este punto tenemos un ArrayList infinito controlado con 2 ArrayList y precio

--- arraylist.java
+++ arraylist.java
@@ -16,16 +16,30 @@ public class arraylist {
  public static void main(String[] args) {
      Scanner in = new Scanner (System.in);
      ArrayList<String> frutas = new ArrayList<>();
+     ArrayList<Integer> precio = new ArrayList<>();
      
-     for (int i=0; i<3; i++){
+     char continuar;
+     int cont = 0;
+     
+     do{
+         
         
-        System.out.println("Ingrese la fruta # "+(i + 1)+": ");
+        System.out.println("Ingrese la fruta # "+(cont + 1)+": ");
         frutas.add(in.nextLine());
+        
+        System.out.println("Ingrese el precio de la "+(frutas.get(cont))+": ");
+        precio.add(in.nextInt());
+        
      
-     }
+        System.out.println("Desea seguir ingresando frutas (s para si y n para no): ");
+        
+        continuar = in.next().charAt(0);
+        in.nextLine();
+        cont++;
+     }while(continuar == 's');
      
-     for (int i=0; i<3; i++){
-     System.out.println("Frutas: "+ frutas.get(i));
+     for (int i=0; i< cont; i++){
+     System.out.println("Frutas: "+ frutas.get(i)+ " precio: "+ precio.get(i));
      }
      
      
